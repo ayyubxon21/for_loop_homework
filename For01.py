34cd4aeb300c94e1e3d6a050601c5f8bde618b1e
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2022-11-01 09:40:02 -0700

    01

--- For01.py
+++ For01.py
@@ -9,4 +9,10 @@ def main(n):
     Returns:
         list: return  answer
     """
-    return
+    k = 0
+    list1=[]
+    while k<n:
+        list1=list1+[k+1]
+        k+=1
+    return list1
+print(main(5))
